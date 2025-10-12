42a24220107ee6507558cf44c59921bc2c00816e
Satish77-hub <satishakula713@gmail.com>
2025-10-12 19:16:47 +0530

    minor bug fixes

--- models/owner.js
+++ models/owner.js
@@ -1,33 +1,41 @@
 const mongoose = require("mongoose");
 
-const workerSchema = new mongoose.Schema({
-  userType: { type: String, default: "worker" },
+const ownerSchema = new mongoose.Schema({
+  
+  userType: { type: String, default: "owner" },
   firstName: String,
   lastName: String,
   email: String,
   phone: String,
   location: String,
-  area: String,
-  serviceType: String, 
-  experience: Number,
-  price: Number, 
-  rateUnit: String,
-  availability: String,
-  description: String,
-  image: String,
+  numProperties: Number,
   password: {
     type: String,
     required: true,
-    select: false
+    select: false // Don't include password by default
   },
+  accountNo: String,
+  upiid: String,
   status: { type: String, enum: ['Active', 'Suspended'], default: "Active" },
-  serviceStatus: { type: String, default: "Available" },
   lastLogin: Date,
-  bookingIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Booking" }],
-  clientIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tenant" }],
-  notificationIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Notification" }], // ADD THIS
-  ratingId: { type: mongoose.Schema.Types.ObjectId, ref: "Rating" },
-  isBooked: { type: Boolean, default: false },
+
+  // New fields
+  propertyIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Property" }],
+  tenantIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tenant" }],
+  rentalId: { type: mongoose.Schema.Types.ObjectId, ref: "Rental" },
+  complaintIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Complaint" }],
+  maintenanceRequestIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "MaintenanceRequest" }],
+  notificationIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Notification" }],
+  rentalAgreementIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "RentalAgreement" }],
+
+  notifications: {
+    email: { type: Boolean, default: true },
+    sms: { type: Boolean, default: true },
+    payment: { type: Boolean, default: true },
+    complaint: { type: Boolean, default: true },
+    maintenance: { type: Boolean, default: true }
+  },
+
 }, { timestamps: true });
 
-module.exports = mongoose.model("Worker", workerSchema);
+module.exports = mongoose.model("Owner", ownerSchema);
